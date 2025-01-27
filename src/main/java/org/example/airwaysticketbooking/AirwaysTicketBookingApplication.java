71fecef8351f273e6cc6c3979c0b5284d3b12a4b
bekk-01 <oybekolimov001@gmail.com>
2025-01-27 20:29:28 +0500

    start

---
+++ src/main/java/org/example/airwaysticketbooking/AirwaysTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package org.example.airwaysticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwaysTicketBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirwaysTicketBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/airwaysticketbooking/AirwaysTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.airwaysticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwaysTicketBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
